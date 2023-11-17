622b544f671580b2c556350030da44f671d3e4ba
wzhjy <wangzhjy@qq.com>
2023-11-16 21:40:04 -0800

    feat: add promise.all

---
+++ promise/all.js
@@ -0,0 +1,79 @@
+
+function isIterable(input) {
+  return typeof input?.[Symbol.iterator] === "function";
+}
+
+Promise.all = function (iterable) {
+  if (!isIterable(iterable)) {
+    throw new TypeError(`${iterable} is not iterable`);
+  }
+  return new Promise(function (resolve, reject) {
+    const result = [];
+    const len = iterable.length;
+    if (len === 0) {
+      return resolve(result);
+    }
+    let counter = 0;
+    [...iterable].forEach((promise, index) => {
+      Promise.resolve(promise).then(function (x) {
+        // fulfills when all of the input's promises fulfill, with an array of the fulfillment values
+        // in the order of the promises passed, regardless of completion order. 
+        result[index] = x;
+        counter++;
+        if (counter === len) {
+          resolve(result);
+        }
+      }, reject); // rejects when any of the input's promises rejects, with this first rejection reason.
+    })
+  });
+}
+
+function test0() {
+  console.log(Promise.all([]));
+}
+
+test0();
+
+function test1() {
+  const promise1 = Promise.resolve(3);
+  const promise2 = 42;
+  const promise3 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 100, 'foo');
+  });
+  const promise4 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 50, 'bar');
+  });
+
+  Promise.all([promise1, promise2, promise3, promise4]).then((values) => {
+    console.log(values);
+  });
+  // Expected output: Array [3, 42, "foo", "bar"]
+}
+
+test1();
+
+function test2() {
+  // All values are non-promises, so the returned promise gets fulfilled
+  const p = Promise.all([1, 2, 3]);
+  // The only input promise is already fulfilled,
+  // so the returned promise gets fulfilled
+  const p2 = Promise.all([1, 2, 3, Promise.resolve(444)]);
+  // One (and the only) input promise is rejected,
+  // so the returned promise gets rejected
+  const p3 = Promise.all([1, 2, 3, Promise.reject(555)]);
+
+  // Using setTimeout, we can execute code after the queue is empty
+  setTimeout(() => {
+    console.log(p);
+    console.log(p2);
+    console.log(p3);
+  });
+
+  // Logs:
+  // Promise { <state>: "fulfilled", <value>: Array[3] }
+  // Promise { <state>: "fulfilled", <value>: Array[4] }
+  // Promise { <state>: "rejected", <reason>: 555 }
+
+}
+
+test2();
