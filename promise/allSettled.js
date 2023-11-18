9dae12d1bc0f847ca05bafe0e87e15534b9cb60a
wzhjy <wangzhjy@qq.com>
2023-11-17 21:52:54 -0800

    feat: add test cases

--- promise/allSettled.js
+++ promise/allSettled.js
@@ -33,3 +33,21 @@ if (!Promise.allSettled) {
     })
   }
 }
+
+//test cases
+function test0() {
+  Promise.allSettled([
+    Promise.resolve(33),
+    new Promise((resolve) => setTimeout(() => resolve(66), 0)),
+    99,
+    Promise.reject(new Error("an error")),
+  ]).then((values) => console.log(values));
+  // [
+  //   { status: 'fulfilled', value: 33 },
+  //   { status: 'fulfilled', value: 66 },
+  //   { status: 'fulfilled', value: 99 },
+  //   { status: 'rejected', reason: Error: an error }
+  // ]
+}
+
+
